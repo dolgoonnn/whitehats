168f4dc1d859db4605fe689110f00d460b029409
Dolgoon <83904398+dolgoonnn@users.noreply.github.com>
2022-12-23 20:03:23 +0800

    initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,9 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import "../styles/utility-patterns.css";
+import "../styles/range-slider.css";
+import "../styles/toggle-switch.css";
+// import "../styles/theme.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    return <Component {...pageProps} />;
 }
