a3b3f7f8a2adee9a87f8029256e81f565a558d5e
Dolgoon <dolgoonnn@Dolgoons-MacBook-Pro.local>
2023-09-25 12:10:29 +0800

    header link change

--- components/Header.jsx
+++ components/Header.jsx
@@ -18,7 +18,7 @@ function Header() {
 
   return (
     <header className={`fixed w-full z-30 md:bg-opacity-90 transition duration-300 ease-in-out ${!top && 'bg-white backdrop-blur-sm shadow-lg'}`}>
-      <div className="max-w-6xl mx-auto px-5 sm:px-6">
+      <div className="max-w-6xl px-5 mx-auto sm:px-6">
         <div className="flex items-center justify-between h-16 md:h-20">
 
           {/* Site branding */}
@@ -41,14 +41,14 @@ function Header() {
 
           {/* Site navigation */}
           <nav className="flex flex-grow">
-            <ul className="flex flex-grow justify-end flex-wrap items-center">
+            <ul className="flex flex-wrap items-center justify-end flex-grow">
               {/* <li>
-                <Link href="/signin" className="font-medium text-gray-600 hover:text-gray-900 px-5 py-3 flex items-center transition duration-150 ease-in-out">Sign in</Link>
+                <Link href="/signin" className="flex items-center px-5 py-3 font-medium text-gray-600 transition duration-150 ease-in-out hover:text-gray-900">Sign in</Link>
               </li> */}
               <li>
-                <Link href="https://forms.gle/Qdfk5EJ3yJwamuvv5" className="btn-sm text-gray-200 bg-gray-900 hover:bg-gray-800 ml-3">
+                <Link href="https://docs.google.com/forms/d/e/1FAIpQLSc9W4t2f3k9odJxWRnIIpGMtMwvNPFTMxprotq2F8RxKck-xw/viewform" className="ml-3 text-gray-200 bg-gray-900 btn-sm hover:bg-gray-800">
                   <span>Бүртгүүлэх</span>
-                  <svg className="w-3 h-3 fill-current text-gray-400 flex-shrink-0 ml-2 -mr-1" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
+                  <svg className="flex-shrink-0 w-3 h-3 ml-2 -mr-1 text-gray-400 fill-current" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
                     <path d="M11.707 5.293L7 .586 5.586 2l3 3H0v2h8.586l-3 3L7 11.414l4.707-4.707a1 1 0 000-1.414z" fillRule="nonzero" />
                   </svg>
                 </Link>
